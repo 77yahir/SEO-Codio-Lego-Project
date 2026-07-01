e35473cda56d5e504fedd1024b9a9a48980bf1ed
77yahir <77yahir80@gmail.com>
2026-07-01 13:57:39 -0700

    Troubleshooting key still

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 def get_api_key():
   load_dotenv()
   api_key = os.environ.get("REBRICKABLE_API_KEY")
